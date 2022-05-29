f91efe34f0b5c1c715360cae11b2de17f9ac0fb6
jonyyy1 <jonathan.quilca@utec.edu.pe>
2022-05-28 21:24:42 -0500

    avance en el archivo mundoXYZ

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from functions.minegraft import dibujar_mundo
+from colorama import init
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+iniciar = input("escribe init para iniciar el juego : ")
+while iniciar != 'init':
+    iniciar = input("escribe init para iniciar el juego : ")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(dibujar_mundo())
