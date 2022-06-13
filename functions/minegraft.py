89fb5ee6702ea299a0a5b77d7dd04832c8330783
jonyyy1 <jonathan.quilca@utec.edu.pe>
2022-06-12 20:43:15 -0500

    avance las acciones right y left del jugador

--- functions/minegraft.py
+++ functions/minegraft.py
@@ -1,28 +1,24 @@
 from colorama import *
+from functions.matriz_juego import *
 
-colores = ['RED', 'BLUE', 'YELLOW', 'CYAN', 'WHITE']
-juego = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 1, 1, 1],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 1, 0, 1, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0],
-         [0, 0, 0, 2, 2, 2, 0, 0, 0, 2, 2, 2, 0, 0, 0, 2, 2, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 0, 0, 3, 0, 0, 2, 0, 0, 0, 2, 0],
-         [0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 2, 2, 2, 0, 2, 2, 2],
-         [0, 0., 0, 0, 3, 0, 0, 5, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 3, 0],
-         [0, 0., 0, 0, 3, 0, 5, 5, 5, 0, 3, 0, 0, 0, 0, 5, 3, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 3, 0],
-         [0, 0., 0, 0, 3, 0, 5, 5, 5, 0, 3, 5, 0, 0, 5, 5, 3, 0, 0, 5, 0, 3, 0, 0, 3, 0, 0, 0, 3, 5],
-         [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 7, 7, 7, 2, 2],
-         [8, 8, 8, 8, 5, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7, 7, 7, 8, 8],
-         [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 5, 8, 5, 7, 7, 7, 8, 8],
-         [8, 8, 8, 8, 8, 8, 5, 8, 8, 5, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 5, 7, 7, 7, 8, 8],
-         [8, 5, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 5, 7, 7, 7, 8, 8],
-         [8, 8, 8, 8, 5, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 5, 8, 8, 8, 8, 8],
-         [8, 8, 8, 8, 8, 8, 5, 8, 8, 8, 8, 8, 8, 8, 5, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 5, 8],
-         [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
-         ]
+# default player position PJ(x,y) // 2,11
+# 11 is the row
+# 2 is the column
+x = 2 - 1
+y = 11 - 1
 
+def draw_player():
+    # 2, 11
+    global x, y
+    # draw body
+    juego[y - 1][x] = 4
+    juego[y][x] = 4
+    juego[y + 1][x] = 4
+    # draw_selection
+    juego[y][x + 1] = 6
+
+
+draw_player()
 
 
 def draw_world():
@@ -30,7 +26,7 @@ def draw_world():
         for j in range(0, 30):
             if juego[i][j] == 0:
                 print(Back.LIGHTWHITE_EX + '   ', end="")
-                print(Style.RESET_ALL+'', end="")
+                print(Style.RESET_ALL + '', end="")
             elif juego[i][j] == 1:
                 print(Back.YELLOW + '   ', end="")
                 print(Style.RESET_ALL + '', end="")
@@ -59,42 +55,81 @@ def draw_world():
         print("")
     return ""
 
-def draw_player(x,y):
-    #11,2
-    x = x-1
-    y = y-1
-    juego[y-1][x] = 4
-    juego[y][x] = 4
-    juego[y + 1][x] = 4
-def draw_selection(x,y):
-    x = x - 1
-    y = y - 1
-    juego[y][x]= 6
+
+def move_player(instructions):
+    # instructions could be : right, left, top, down
+    instructions = instructions.split(" ")
+    print(instructions)
+    # position
+    # 2,11
+    # 3,11
+    global x, y
+    print("x", x)
+    print("y", y)
+
+    if instructions != ['']:
+        for i, v in enumerate(instructions):
+            '''saber si el muñeco esta mirando hacia la derecha o izquierda
+            True -> mirando hacia la derecha
+            False -> mirando hacia la izquierda'''
+
+            if juego[y][x + 1] == 6:
+                look_right = True
+            else:
+                look_right = False
+
+            if look_right:
+                juego[y][x + 1] = 0
+                juego[y - 1][x] = 0
+                juego[y][x] = 0
+                juego[y + 1][x] = 0
+            else:
+                juego[y][x - 1] = 0
+                juego[y - 1][x] = 0
+                juego[y][x] = 0
+                juego[y + 1][x] = 0
+
+            if v == "right":
+                print("entro a right")
+                if look_right:
+                    juego[y - 1][x + 1] = 4
+                    juego[y][x + 1] = 4
+                    juego[y + 1][x + 1] = 4
+                    juego[y][x + 2] = 6
+                    x = x + 1
+                else:
+                    juego[y - 1][x] = 4
+                    juego[y][x] = 4
+                    juego[y + 1][x] = 4
+                    juego[y][x + 1] = 6
 
 
-def move_player(cadena):
-    #right left top down
-    cadena = cadena.split(" ")
-    #posicion
-    #2,11
-    #3,11
-    x = 2-1
-    y = 11-1
-    juego[y -1][x] = 0
-    juego[y][x] = 0
-    juego[y +1][x] = 0
-    juego[y][x+1] = 0
-    for i in cadena:
-        if i == "right":
-            print("entro")
-            juego[y-1][x+1]=4
-            juego[y][x+1]=4
-            juego[y+1][x+1]=4
-            juego[y][x+2] = 6
-        elif i == "left":
-            print("left")
-        elif i == "top":
-            print("top")
-        elif i == "down":
-            print("down")
+            elif v == "left":
+                print("entro a left")
+                print(juego[y][x + 1])
+                # si cumple esto es porque el muñeco esta a la mirando hacia la derecha
+                if look_right:
+                    juego[y - 1][x] = 4
+                    juego[y][x] = 4
+                    juego[y + 1][x] = 4
+                    juego[y][x - 1] = 6
+                else:  # esta mirando hacia la izquierda
+                    juego[y - 1][x - 1] = 4
+                    juego[y][x - 1] = 4
+                    juego[y + 1][x - 1] = 4
+                    juego[y][x - 2] = 6
+                    x = x - 1
 
+            elif v == "top":
+                juego[y - 2][x] = 4
+                juego[y - 1][x] = 4
+                juego[y][x] = 4
+                if juego[x + 1] == 6:
+                    juego[y - 1][x + 1] = 6
+                else:
+                    juego[y - 1][x - 1] = 6
+                print("top")
+            elif v == "down":
+                print("down")
+
+        draw_world()
--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from colorama import init
 iniciar = input("escribe init para iniciar el juego : ")
 while iniciar != 'init':
     iniciar = input("escribe init para iniciar el juego : ")
-
-print(draw_selection(3, 11))
-print(draw_player(2, 11))
-
-move_player = move_player("right ")
 print(draw_world())
+
+
+move_player = move_player("right right left left left right")
+
+
+
+
+
