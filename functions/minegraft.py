50c73c1bb2d0c3c3410ace814f9ba75f040bcf5b
jonyyy1 <jonathan.quilca@utec.edu.pe>
2022-06-15 18:03:22 -0500

    se arreglaron bugs del movimiento rgiht y left solucionado al 100%

--- functions/minegraft.py
+++ functions/minegraft.py
@@ -6,22 +6,58 @@ from functions.matriz_juego import *
 # 2 is the column
 x = 2 - 1
 y = 11 - 1
+wood_blocks= 0
+stone_blocks = 0
+estado_mira = "undefined"
+bloque_destruido = [-1, -1]
+look_right = False
+changed = False
+
 
 def draw_player():
     # 2, 11
-    global x, y
-    # draw body
-    juego[y - 1][x] = 4
-    juego[y][x] = 4
-    juego[y + 1][x] = 4
-    # draw_selection
-    juego[y][x + 1] = 6
+    global x, y, estado_mira, changed
 
+    if estado_mira == "right":
 
-draw_player()
+        # draw_selection
+        # draw body
+        juego[y - 1][x] = 4
+        juego[y][x] = 4
+        juego[y + 1][x] = 4
+        # draw_selection
+        juego[y][x + 1] = 6
+
+    elif estado_mira == "left":
+        # draw body
+        juego[y - 1][x] = 4
+        juego[y][x] = 4
+        juego[y + 1][x] = 4
+        # draw_selection
+        juego[y][x - 1] = 6
+    elif estado_mira == "destroy" or  estado_mira == "collect":
+        juego[y - 1][x] = 4
+        juego[y][x] = 4
+        juego[y + 1][x] = 4
+        # draw_selection
+        juego[y][x + 1] = 6
+        # draw_selection
+        changed = True
+    else:
+        juego[y - 1][x] = 4
+        juego[y][x] = 4
+        juego[y + 1][x] = 4
+        # draw_selection
+        juego[y][x + 1] = 6
 
 
 def draw_world():
+    global x, y, look_right, changed, wood_blocks, stone_blocks
+    draw_player()
+    if juego[y][x + 1] == 6:
+        look_right = True
+    else:
+        look_right = False
     for i in range(0, 20):
         for j in range(0, 30):
             if juego[i][j] == 0:
@@ -51,85 +87,46 @@ def draw_world():
             elif juego[i][j] == 8:
                 print(Back.BLACK + '   ', end="")
                 print(Style.RESET_ALL + '', end="")
-
         print("")
-    return ""
+    print("REPORTE")
+    print(" WOOD BLOCKS = ", wood_blocks)
+    print(" STONE BLOCKS = ", stone_blocks)
+    for i in range(0, 20):
+        for j in range(0, 30):
+            juego[i][j] = copia_juego[i][j]
 
+    if changed:
+        copia_juego[y][x + 1] = 0
+        changed = False
+    return ""
 
 def move_player(instructions):
     # instructions could be : right, left, top, down
     instructions = instructions.split(" ")
-    print(instructions)
+
     # position
     # 2,11
     # 3,11
-    global x, y
-    print("x", x)
-    print("y", y)
+    global x, y, muñeco_actual, estado_mira, wood_blocks, stone_blocks
+
 
     if instructions != ['']:
         for i, v in enumerate(instructions):
-            '''saber si el muñeco esta mirando hacia la derecha o izquierda
-            True -> mirando hacia la derecha
-            False -> mirando hacia la izquierda'''
-
-            if juego[y][x + 1] == 6:
-                look_right = True
-            else:
-                look_right = False
-
-            if look_right:
-                juego[y][x + 1] = 0
-                juego[y - 1][x] = 0
-                juego[y][x] = 0
-                juego[y + 1][x] = 0
-            else:
-                juego[y][x - 1] = 0
-                juego[y - 1][x] = 0
-                juego[y][x] = 0
-                juego[y + 1][x] = 0
-
             if v == "right":
-                print("entro a right")
-                if look_right:
-                    juego[y - 1][x + 1] = 4
-                    juego[y][x + 1] = 4
-                    juego[y + 1][x + 1] = 4
-                    juego[y][x + 2] = 6
-                    x = x + 1
-                else:
-                    juego[y - 1][x] = 4
-                    juego[y][x] = 4
-                    juego[y + 1][x] = 4
-                    juego[y][x + 1] = 6
-
-
+                estado_mira = "right"
+                x = x + 1
             elif v == "left":
-                print("entro a left")
-                print(juego[y][x + 1])
-                # si cumple esto es porque el muñeco esta a la mirando hacia la derecha
-                if look_right:
-                    juego[y - 1][x] = 4
-                    juego[y][x] = 4
-                    juego[y + 1][x] = 4
-                    juego[y][x - 1] = 6
-                else:  # esta mirando hacia la izquierda
-                    juego[y - 1][x - 1] = 4
-                    juego[y][x - 1] = 4
-                    juego[y + 1][x - 1] = 4
-                    juego[y][x - 2] = 6
-                    x = x - 1
-
-            elif v == "top":
-                juego[y - 2][x] = 4
-                juego[y - 1][x] = 4
-                juego[y][x] = 4
-                if juego[x + 1] == 6:
-                    juego[y - 1][x + 1] = 6
-                else:
-                    juego[y - 1][x - 1] = 6
-                print("top")
-            elif v == "down":
-                print("down")
-
+                estado_mira = "left"
+                x = x - 1
+            elif v == "destroy":
+                estado_mira = "destroy"
+                bloque_destruido[0] = y
+                bloque_destruido[1] = x
+                copia_juego[y][x + 1] = 0
+            elif v == "collect":
+                estado_mira = "destroy"
+                bloque_destruido[0] = y
+                bloque_destruido[1] = x
+                copia_juego[y][x + 1] = 0
+                wood_blocks += 1
         draw_world()
--- main.py
+++ main.py
@@ -6,8 +6,12 @@ while iniciar != 'init':
     iniciar = input("escribe init para iniciar el juego : ")
 print(draw_world())
 
-
-move_player = move_player("right right left left left right")
+#una vez inicializado el programa se le pedira al usuario ingresar que accion quiere hace
+accion = input("que accion deseas realizar : ")
+move_player(accion)
+while accion != '0':
+    accion = input("que accion deseas realizar : ")
+    move_player(accion)
 
 
 
